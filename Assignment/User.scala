8f7a013dfb031b27712fc046cd8859895c12a776
Nik Fikri <nikfikrics@gmail.com>
2018-12-05 21:38:01 +0800

    Updated comments
    Minor edit.

--- Assignment/User.scala
+++ Assignment/User.scala
@@ -6,10 +6,10 @@ class User() extends ArcanaList {
   // Method to add the user's balance which takes amount as parameter.
   def userAddFund(amount: Double): Unit = {
     userBalance = userBalance + amount
-  } //End userAddFund method.
+  } // End userAddFund method.
 
   // Method to deduct the user's balance when they checkout.
   def deductAmount(amount: Double): Unit = {
     userBalance = userBalance - amount
-  } //End deductAmount method.
+  } // End deductAmount method.
 } // End class User.
