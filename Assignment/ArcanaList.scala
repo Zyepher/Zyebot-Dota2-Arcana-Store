010f147311a8d0c10a449e9acac983cb141d3258
Nik Fikri <nikfikrics@gmail.com>
2021-11-26 12:03:03 +0800

    Delete Assignment directory

--- Assignment/ArcanaList.scala
+++
@@ -1,9 +0,0 @@
-package Assignment
-
-import scalafx.collections.ObservableBuffer
-
-/* A trait that is applicable for Store object, ShoppingCart class, and User class
-// since all these three classes need to have their own list of Arcanas */
-trait ArcanaList {
-  var arcanaList: ObservableBuffer[Arcana] = ObservableBuffer[Arcana]()
-}
